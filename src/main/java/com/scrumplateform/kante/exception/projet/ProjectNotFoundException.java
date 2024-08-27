a0b777792e093b4d35a346af3e98ccf3b07a9d7e
Yoan <harena@kanteco.com>
2024-08-27 18:33:10 +0300

    project detail

---
+++ src/main/java/com/scrumplateform/kante/exception/projet/ProjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.scrumplateform.kante.exception.projet;
+
+public class ProjectNotFoundException extends RuntimeException {
+    public ProjectNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/scrumplateform/kante/service/projet/ProjetService.java
+++ src/main/java/com/scrumplateform/kante/service/projet/ProjetService.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import com.scrumplateform.kante.exception.projet.ProjectNotFoundException;
 import com.scrumplateform.kante.model.projet.Projet;
 import com.scrumplateform.kante.repository.projet.ProjetRepository;
 
@@ -14,6 +16,11 @@ public class ProjetService {
 
     @Autowired
     private ProjetRepository projetRepository;
+
+    public Projet getProjetById(String projetId) {
+        return projetRepository.findById(projetId)
+                .orElseThrow(() -> new ProjectNotFoundException("Projet avec ID " + projetId + " n'existe pas."));
+        }
 
     public Page<Projet> getPaginatedProjects(String scrumId, String keyword, int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "dateCreation"));
