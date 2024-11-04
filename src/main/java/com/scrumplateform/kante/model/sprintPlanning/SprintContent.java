7b21559ff61f28cf3a6e46eb6dcf7cdfc19303c3
Yoan <harena@kanteco.com>
2024-11-04 16:07:58 +0300

    Sprint planning
    une tache doit avoir un titre, une description, priorité
    modifier => detail
    les taches ordonnées par priorité
    une fois tache checked, tsy afaka checked intsony + modal validation

--- src/main/java/com/scrumplateform/kante/model/sprintPlanning/SprintContent.java
+++ src/main/java/com/scrumplateform/kante/model/sprintPlanning/SprintContent.java
@@ -13,10 +13,12 @@ import lombok.Data;
 @Document(collection = "sprintContent")
 public class SprintContent {
     private String id;
+    private String titre;
     private String description;
     private int ordre;
     private Date dateDebut;
     private Date dateFin;
     private SprintCheck status;
+    private int priorite = 1;
 }
 
